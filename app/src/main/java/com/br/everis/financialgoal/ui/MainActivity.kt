f8850451d4b989d58c5e5478bbfc95d29194e1b3
natanbrito <natanfelipe@gmail.com>
2021-06-14 15:02:53 -0300

    Estrutura inicial

--- app/src/main/java/com/br/everis/financialgoal/ui/MainActivity.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.br.everis.financialgoal
+package com.br.everis.financialgoal.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.br.everis.financialgoal.R
 
 class MainActivity : AppCompatActivity() {
 
