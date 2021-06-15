9458637a1c26249cafb37598aaff6d6b7558bf41
andersonms92 <anderson.matos.santos@everis.com>
2021-06-15 10:35:12 -0300

    fix(EFG-29): Correção dos indicadores do Carousel

--- app/src/main/java/com/br/everis/financialgoal/ui/ViewPageAdapter.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/ViewPageAdapter.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.viewpager.widget.PagerAdapter
 import com.br.everis.financialgoal.R
 import java.util.*
