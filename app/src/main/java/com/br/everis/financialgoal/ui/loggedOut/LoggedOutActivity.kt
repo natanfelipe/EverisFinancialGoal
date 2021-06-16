3836748bc8fa0b95f6d981dfe3812ad10eff4f35
andersonms92 <anderson.matos.santos@everis.com>
2021-06-16 15:48:32 -0300

    feat(EFG-29): Criação das funções de click

--- app/src/main/java/com/br/everis/financialgoal/ui/loggedOut/LoggedOutActivity.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/loggedOut/LoggedOutActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.br.everis.financialgoal.R
@@ -12,29 +14,29 @@ import com.br.everis.financialgoal.ui.cadastro.CadastroActivity
 import com.google.android.material.tabs.TabLayout
 import java.util.*
 
-class LoggedOutActivity : AppCompatActivity() {
+class LoggedOutActivity : AppCompatActivity(), View.OnClickListener {
 
-    var currentPage = 0
+    private var currentPage = 0
     private lateinit var timer: Timer
     private lateinit var btnOpenAccount: Button
-    private lateinit var btnLogin:Button
+    private lateinit var btnLogin: Button
 
     companion object {
         const val DELAY_MS: Long = 500
         const val PERIOD_MS: Long = 3000
     }
 
-    var mViewPager: ViewPager? = null
+    private var mViewPager: ViewPager? = null
     private var tabLayout: TabLayout? = null
 
-    var images = intArrayOf(
+    private var images = intArrayOf(
         R.drawable.logged_out_slide_1,
         R.drawable.logged_out_slide_2,
         R.drawable.logged_out_slide_3,
         R.drawable.logged_out_slide_4,
     )
 
-    var NUM_PAGES = images.size
+    private var NUM_PAGES = images.size
 
     private var mViewPagerAdapter: ViewPagerAdapter? = null
 
@@ -44,14 +46,7 @@ class LoggedOutActivity : AppCompatActivity() {
 
         setView()
         setAdapter()
-        btnOpenAccount.setOnClickListener {
-            startActivity(Intent(this,CadastroActivity::class.java))
-        }
-
-
-//        btnLogin.setOnClickListener {
-//
-//        }
+        click()
     }
 
     private fun setView() {
@@ -78,5 +73,24 @@ class LoggedOutActivity : AppCompatActivity() {
                 handler.post(update)
             }
         }, DELAY_MS, PERIOD_MS)
+    }
+
+    private fun click() {
+        btnOpenAccount.setOnClickListener(this)
+        btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
+        when (v.id) {
+            R.id.btn_login -> {
+                Toast.makeText(this, "Não implementado", Toast.LENGTH_SHORT).show()
+            }
+            R.id.btn_open_account -> {
+                startActivity(Intent(this, CadastroActivity::class.java))
+            }
+            else -> {
+            }
+        }
     }
 }
