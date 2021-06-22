f41c0d11aaa4842f3dd67872350e875c2041ff5b
andersonms92 <anderson.matos.santos@everis.com>
2021-06-21 22:18:35 -0300

    feat[EFG-66]: criando alertdialog para email inválido

--- app/src/main/java/com/br/everis/financialgoal/ui/cadastro/fragment/EmailFragment.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/cadastro/fragment/EmailFragment.kt
@@ -2,21 +2,22 @@ package com.br.everis.financialgoal.ui.cadastro.fragment
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatImageView
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.DialogFragment
 import com.br.everis.financialgoal.R
-import com.br.everis.financialgoal.ui.login.fragment.EmailAlertDialog
 
-class EmailFragment : Fragment() {
+class EmailFragment : DialogFragment() {
 
+    private lateinit var edtEmail: EditText
     private lateinit var btnContinuar:Button
     private lateinit var btnBackNavBar:AppCompatImageView
     private lateinit var alertDialog: AlertDialog.Builder
-    private val dialog = EmailAlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,28 +30,24 @@ class EmailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        alertDialog = AlertDialog.Builder(view.context)
-        val factory = LayoutInflater.from(view.context)
-        val viewer = factory.inflate(R.layout.fragment_dialogup_error, null)
-        alertDialog.setView(viewer)
-        alertDialog.setNegativeButton(
-            "OK"
-        ) { dialog, which -> dialog.dismiss() }
-        alertDialog.show()
-
         setView(view)
         setClick()
     }
 
     private fun setClick() {
-        btnContinuar.setOnClickListener {
 
-//            parentFragmentManager.beginTransaction().apply {
-//                replace(R.id.fragment,NomeFragment.newInstance())
-//                addToBackStack(null)
-//                commit()
-//            }
+        btnContinuar.setOnClickListener {
+            if(!isBoolean()){
+                parentFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragment, NomeFragment.newInstance())
+                    addToBackStack(null)
+                    commit()
+                }
+            } else {
+                onAlertDialog(it)
+            }
         }
+
         btnBackNavBar.setOnClickListener {
             activity?.finish()
         }
@@ -59,11 +56,30 @@ class EmailFragment : Fragment() {
     private fun setView(view: View) {
         btnContinuar = view.findViewById(R.id.btn_cadastro_email)
         btnBackNavBar = view.findViewById(R.id.btn_back_cadastro)
+        edtEmail = view.findViewById(R.id.edt_email)
+    }
 
+    private fun onAlertDialog(view: View) {
+
+        alertDialog = AlertDialog.Builder(view.context)
+        alertDialog.setTitle(TITLE)
+        alertDialog.setMessage(MESSAGE)
+            .setCancelable(false)
+            .setPositiveButton(POSITIVE_BUTTON) { dialog, id ->
+                dialog.dismiss()
+            }
+        alertDialog.show()
+    }
+
+    private fun isBoolean() : Boolean {
+        return TextUtils.isEmpty(edtEmail.text.toString())
     }
 
     companion object {
         fun newInstance() = EmailFragment()
+        const val TITLE = "E-mail inválido"
+        const val MESSAGE = "forneça um endereço de e-mail válido"
+        const val POSITIVE_BUTTON = "OK"
     }
 
 }
