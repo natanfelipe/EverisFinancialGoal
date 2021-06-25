9f2be42c225414fefb37b4972ea4d07e389b8dc4
Vinicius Bianco <viniciusbiancorodrigues1243@gmail.com>
2021-06-25 12:29:21 -0300

    Integração e finalização do recovery

--- app/src/main/java/com/br/everis/financialgoal/repository/recovery/ImpRecoveryRepository.kt
+++ app/src/main/java/com/br/everis/financialgoal/repository/recovery/ImpRecoveryRepository.kt
@@ -3,7 +3,7 @@ package com.br.everis.financialgoal.repository.recovery
 import com.br.everis.financialgoal.data.datarecoverysource.model.RecoveryModelRequest
 import com.br.everis.financialgoal.data.datarecoverysource.recovery.ImpRecoveryDataSource
 import com.br.everis.financialgoal.data.datarecoverysource.recovery.RecoveryResult
-import com.br.everis.financialgoal.data.datasource.worker.cadastro.ImpCadastroDataSource
+
 
 class ImpRecoveryRepository (
     private val dataRecoverySource: ImpRecoveryDataSource
--- app/src/main/java/com/br/everis/financialgoal/ui/login/fragment/MyFragment.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/login/fragment/MyFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.br.everis.financialgoal.R
+import com.br.everis.financialgoal.data.datarecoverysource.model.RecoveryModelRequest
+import com.br.everis.financialgoal.data.datarecoverysource.model.RecoveryModelResponse
+import com.br.everis.financialgoal.repository.recovery.RecoveryRepository
+import com.br.everis.financialgoal.viewmodel.recovery.RecoveryViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MyFragment : DialogFragment() {
 
     private lateinit var btnOk:TextView
     private lateinit var btnCancelar:TextView
+    private lateinit var edtEmail:EditText
+    private val recoveryViewModel: RecoveryViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,17 +33,33 @@ class MyFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         btnOk = view.findViewById(R.id.ok)
         btnCancelar = view.findViewById(R.id.cancelar)
+        edtEmail = view.findViewById(R.id.email)
 
         setClick()
     }
 
     private fun setClick() {
         btnOk.setOnClickListener {
-            dialog?.cancel()
+            val email = edtEmail.text.toString()
+           setViewModel(email)
         }
         btnCancelar.setOnClickListener {
             dialog?.cancel()
         }
+    }
+
+    private fun setViewModel(email:String) {
+    val recoveryModelRequest = RecoveryModelRequest(
+        username = email
+    )
+    recoveryViewModel.initialize(recoveryModelRequest)
+    recoveryViewModel.response.observe(viewLifecycleOwner){
+        if (it.res){
+            dialog?.cancel()
+        }else{
+            dialog?.cancel()
+        }
+    }
     }
 
 }
