2f37239165997867eafa6bc23f06d25b27ce6f1e
joseRobertoMoura <jose.roberto.moura@everis.com>
2021-06-29 14:31:09 -0300

    finalização layout

--- app/src/main/java/com/br/everis/financialgoal/ui/home/HomeActivity.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/home/HomeActivity.kt
@@ -1,12 +1,15 @@
 package com.br.everis.financialgoal.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.br.everis.financialgoal.R
+import com.br.everis.financialgoal.ui.login.LoginActivity
 import com.br.everis.financialgoal.utils.preferences.AppPreferences
+import com.br.everis.financialgoal.utils.sessionManagment.SessionManagement
 
 class HomeActivity : AppCompatActivity() {
 
@@ -15,16 +18,15 @@ class HomeActivity : AppCompatActivity() {
     }
 
     lateinit var appPreferences: AppPreferences
-    lateinit var bundle: Bundle
     lateinit var tvNameHome:TextView
     lateinit var cardUltimaSimulacao:ConstraintLayout
     lateinit var cardCalculadora:ConstraintLayout
     lateinit var cardSimulador:ConstraintLayout
+    lateinit var sessionManagement: SessionManagement
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
         appPreferences = AppPreferences(this)
 
         initView()
@@ -48,12 +50,18 @@ class HomeActivity : AppCompatActivity() {
         tvNameHome.text = appPreferences.getString(KEY_NICK_NAME)
     }
 
-
     private fun initView() {
-        bundle = Bundle()
         tvNameHome = findViewById(R.id.tv_name_home)
         cardUltimaSimulacao = findViewById(R.id.card_ultima_simulacao)
         cardCalculadora = findViewById(R.id.card_calculadora)
         cardSimulador = findViewById(R.id.card_simulador)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        sessionManagement = SessionManagement(this)
+        sessionManagement.finishSession()
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
     }
 }
