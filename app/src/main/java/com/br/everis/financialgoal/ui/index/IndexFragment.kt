c5a9337b1abd1583e333535b9868ef0ab27ddbb5
agathaappb <agatha.alana@hotmail.com>
2021-07-05 16:28:25 -0300

    Subindo spinner ok

--- app/src/main/java/com/br/everis/financialgoal/ui/index/IndexFragment.kt
+++ app/src/main/java/com/br/everis/financialgoal/ui/index/IndexFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import com.br.everis.financialgoal.R
 import com.br.everis.financialgoal.ui.monthly.MonthlyFragment
@@ -14,6 +16,8 @@ import com.br.everis.financialgoal.ui.monthly.MonthlyFragment
 
 class IndexFragment(private val contextActivity: FragmentActivity) : Fragment() {
     lateinit var  spinnerIndex :Spinner
+    lateinit var text_navBar: TextView
+    lateinit var btn_mudar_indice : Button
 
 
     override fun onCreateView(
@@ -34,9 +38,21 @@ class IndexFragment(private val contextActivity: FragmentActivity) : Fragment() 
 
 
     fun setView(vie: View) {
+        text_navBar = requireView().findViewById(R.id.textNav)
+        text_navBar.text = getString(R.string.nav_bar_monthly)
         spinnerIndex = requireView().findViewById(R.id.spinner_indice)
+        btn_mudar_indice = requireView().findViewById(R.id.btn_mudar_indice)
     }
     private fun setClick() {
+        spinnerIndex.isEnabled = false
+        setSpinner()
+        btn_mudar_indice.setOnClickListener {
+            spinnerIndex.isEnabled = true
+        }
+
+
+    }
+    fun setSpinner(){
         ArrayAdapter.createFromResource(
             contextActivity,
             R.array.indice_correcao,
@@ -45,7 +61,6 @@ class IndexFragment(private val contextActivity: FragmentActivity) : Fragment() 
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerIndex.adapter = adapter
         }
-
     }
 
 
